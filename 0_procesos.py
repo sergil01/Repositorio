7926b9c45060f0bbfbac2838d129a724b1bc66d8
mat <sergil01@ucm.es>
2022-02-03 19:41:27 +0100

    pruebas python

---
+++ 0_procesos.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Feb  3 14:25:12 2022
+
+@author: mat
+"""
+
+from time import sleep
+from random import random
+"""DocumentaciÃ³n multiprocessinghttps://docs.python.org/3.6/library/multiprocessing.html"""
+from multiprocessing import Process
+def f():
+    for i in range(5):
+        print ("hola", i)       
+        sleep(random())
+        
+        
+if __name__ == "__main__":    
+    p = Process(target=f)    
+    q = Process(target=f)    
+    p.start()    
+    q.start()
+    print ("fin")
+    
